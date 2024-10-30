0fef33ecc94448ac454d68820c85afed7f0d78e1
hexzhen3x7 <hexzhen3x7@outlook.de>
2024-10-30 14:07:06 +0100

    Added

--- src/extensions/mcp23017.h
+++ src/extensions/mcp23017.h
@@ -1,8 +1,33 @@
-#ifndef _WPI_MCP23017_H_
-#define _WPI_MCP23017_H_
+/*
+ * 23017.h:
+ *	Extend wiringPi with the MCP 23017 I2C GPIO expander chip
+ *	Copyright (c) 2013 Gordon Henderson
+ ***********************************************************************
+ * This file is part of wiringPi:
+ *	https://projects.drogon.net/raspberry-pi/wiringpi/
+ *
+ *    wiringPi is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    wiringPi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with wiringPi.
+ *    If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************
+ */
 
-  #include "../addon.h"
-  
-  DECLARE_EXPORT_INIT(mcp23017);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+extern int mcp23017Setup (const int pinBase, const int i2cAddress) ;
+
+#ifdef __cplusplus
+}
 #endif
